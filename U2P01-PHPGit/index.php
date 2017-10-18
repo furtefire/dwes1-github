4bd8c938fce429820d13adfb40bbb7b393cef812
Alumno <Alumno@W7Bsica>
2017-10-18 12:46:55 +0200

    parche 3.14 12:46

---
+++ U2P01-PHPGit/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+</head>
+<body>
+<?php
+$nombre="sergio";
+$apellido="anonimo";
+echo "Mi nombre es $nombre mis apellidos son $apellido";
+echo 'acabo de de poner entre "Mi nombre es $nombre mis apellidos son $apellido"';
+
+
+?>
+</body>
+</html>
